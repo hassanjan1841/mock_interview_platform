e7abfa0e581682c6fda1086908192a7fe8a0fa43
hassanjan1841 <hijan29714@gmail.com>
2025-04-25 16:56:01 +0500

    Add new assets and type definitions for feedback and interview management
    - Added various cover images for services (adobe, amazon, facebook, hostinger, pinterest, quora, reddit, skype, spotify, telegram, tiktok, yahoo).
    - Introduced a new logo SVG file.
    - Removed the old next.svg file and replaced it with new assets including profile.svg, react.svg, robot.png, star.svg, tailwind.svg, tech.svg, upload.svg, and user-avatar.png.
    - Deleted the old vercel.svg file.
    - Created new TypeScript definitions for feedback, interviews, users, and related parameters in index.d.ts.
    - Added enums and interfaces for message handling in vapi.d.ts.

---
+++ components/AuthForm.tsx
@@ -0,0 +1,107 @@
+"use client";
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+
+import Image from "next/image";
+import Link from "next/link";
+import { toast } from "sonner";
+import FormField from "./FormField";
+import { useRouter } from "next/navigation";
+
+const authFormSchema = (type: FormType) => {
+  return z.object({
+    name:
+      type === "sign-up" ? z.string().min(2).max(50) : z.string().optional(),
+    email: z.string().email(),
+    password: z.string().min(8),
+  });
+};
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  const router = useRouter();
+
+  const formSchema = authFormSchema(type);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      if (type === "sign-in") {
+        toast.success("Sign in successful");
+        router.push("/");
+      } else {
+        toast.success("Sign up successful");
+        router.push("/sign-in");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(`Something went wrong. Please try again. ${error}`);
+    }
+  }
+
+  const isSignIn = type === "sign-in";
+  return (
+    <div className="card-border lg:min-w-[566px]">
+      <div className="flex flex-col gap-6 card py-14 px-10">
+        <div className="flex flex-row gap-2 justify-center ">
+          <Image src={"/logo.svg"} alt="Logo" width={38} height={32} />
+          <h2>PrepWise</h2>
+        </div>
+        <h3>Practice Job Interviews With AI</h3>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-6 w-full mt-4 form"
+          >
+            {!isSignIn && (
+              <FormField
+                name="name"
+                label="name"
+                placeholder="Your Name"
+                control={form.control}
+              />
+            )}
+            <FormField
+              name="email"
+              label="email"
+              placeholder="Your Email"
+              control={form.control}
+            />
+            <FormField
+              name="password"
+              label="password"
+              placeholder="Your Password"
+              type="password"
+              control={form.control}
+            />
+            <Button type="submit" className="btn">
+              {isSignIn ? "Sign In" : "Create an Account"}
+            </Button>
+            <p className="text-center">
+              {isSignIn ? "Don't have an account?" : "Already have an account?"}
+              <Link
+                href={!isSignIn ? "/sign-in" : "/sign-up"}
+                className="font-bold text-user-primary ml-1"
+              >
+                {isSignIn ? "Sign Up" : "Sign In"}
+              </Link>
+            </p>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
